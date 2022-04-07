4746768c36d869dbd6714e6533b36baffd15afc9
Luca Coduri <coduri.vitor@gmail.com>
2022-04-07 15:59:17 +0200

    Added consumption to ships

--- Ships/Ship.cpp
+++ Ships/Ship.cpp
@@ -13,8 +13,15 @@ string Ship::getId() const {
    return "[" + getModel() + to_string(no) + "]";
 }
 
-double Ship::getConsumption() const {
-   return pow(getWeight(), 1.0/3.0) / 2 * log10 (getWeight() * ) ;
+double Ship::getConsumption(double distance) const {
+   return pow(getWeight(), 1.0/3.0) / 2 * log10 (getWeight() * speed) * log10
+   (distance + 1) ;
 }
 
-Ship::Ship(std::string nickname, size_t no):nickname(std::move(nickname)), no(no) {}
+Ship::Ship(std::string nickname, size_t no):nickname(std::move(nickname)), no(no),
+speed(0)
+{}
+
+double Ship::getSpeed() const {
+   return speed;
+}
