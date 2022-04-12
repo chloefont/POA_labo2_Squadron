b2368dd7dd454b4f14c9479b2d0dae18413d77e5
chloefont <cfontaine@citycable.ch>
2022-04-12 16:36:12 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "Squadron.h"
+#include "Ships/Cargo/Cargo.h"
+#include "Ships/Cargo/Shuttle.h"
+#include "Ships/Cargo/StarDreadnought.h"
+#include "Ships/HunterTIE.h"
+#include "Ships/InterceptorTIE.h"
+#include "Ships/Ship.h"
 
 int main() {
-   std::cout << "Hello, World!" << std::endl;
+   HunterTIE blackLeader;
+   blackLeader.setNickname("Black leader");
+   HunterTIE blackTwo;
+   Shuttle shuttle(23.4);
+
+   Squadron squadron("Black Squadron");
    return 0;
 }
